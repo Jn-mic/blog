9283bda4a584123ddb10437a7cfb979fa4ac0bc0
jn-mic <jackotienokey@gmail.com>
2021-06-22 08:35:26 +0300

    solving error on index template for non display

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,4 @@
+
 from flask import render_template, url_for, redirect, request, flash, abort
 from flask_login import current_user, login_required
 
@@ -10,25 +11,11 @@ from app.requests import getQuotes
 
 
 @main.route('/')
-@main.route('/home')
+# @main.route('/home')
 def index():
     quotes = getQuotes()
     posts = Post.query.all()
     return render_template('index.html', quotes=quotes, posts=posts, current_user=current_user)
-
-
-def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pic', picture_fn)
-
-    output_size = (125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-
-    return picture_fn
 
 
 @main.route('/profile', methods=['GET', 'POST'])
